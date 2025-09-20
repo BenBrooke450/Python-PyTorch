62bcbb868a981d13fb42ae5b9768998247515870
BenBrooke450 <bensir@mail.com>
2025-09-20 21:29:37 +0200

    Basic

--- PyTorch for Deep Learning Bootcamp - Udemy/Section 5 - PyTorch Computer Vision/1.CNN model..py
+++ PyTorch for Deep Learning Bootcamp - Udemy/Section 5 - PyTorch Computer Vision/1.CNN model..py
@@ -1,2 +1,83 @@
 
 
+import torch
+from torch import nn
+
+import torchvision
+from torchvision import datasets
+from torchvision import transforms
+from torchvision.transforms import ToTensor
+
+
+train_data = datasets.FashionMNIST(
+    root = "data",
+    train = True,
+    download = True,
+    transform = torchvision.transforms.ToTensor(),
+    target_transform=None)
+
+text_data = datasets.FashionMNIST(
+    root = "data",
+    train = False,
+    download = True,
+    transform = ToTensor(),
+    target_transform=None
+)
+
+
+
+print(len(train_data), len(text_data))
+#60000 10000
+
+
+
+print(train_data.classes)
+#['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+
+
+
+image, label = train_data[0]
+print(image.shape,label)
+#torch.Size([1, 28, 28]) 9
+
+
+
+
+
+print(train_data,text_data)
+"""
+Dataset FashionMNIST
+    Number of datapoints: 60000
+    Root location: data
+    Split: Train
+    StandardTransform
+Transform: ToTensor() Dataset FashionMNIST
+    Number of datapoints: 10000
+    Root location: data
+    Split: Test
+    StandardTransform
+Transform: ToTensor()
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
