991b6cc74e04d4d5db1cd5d1694dec3265aa8e25
BenBrooke450 <bensir@mail.com>
2025-11-30 20:44:32 +0100

    Basic

--- PyTorch for Deep Learning Bootcamp - Udemy/Section 1 - Introduction/Info.md
+++
@@ -1,6 +0,0 @@
-
-
-https://zerotomastery.io/cheatsheets/pytorch-cheat-sheet/
-
-
-https://github.com/mrdbourke/pytorch-deep-learning
--- PyTorch for Deep Learning Bootcamp - Udemy/Section 7 - PyTorch Going Modular/Package Module/module_nn/__init__.py
+++
@@ -1,5 +0,0 @@
-
-
-from .data_setup import create_dataloaders
-from .engine import train_step, test_step, train
-from .model_builder import TinyVGG
